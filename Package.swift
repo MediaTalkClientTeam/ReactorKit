4e63ad6035619dee33c32dd86979d6d062a0ad3b
ChangGwiLee <lcg5450@gmail.com>
2020-03-11 15:30:19 +0900

    - fix repo
    "github.com/lcg5450" -> "github.ncsoft.com/MediaTalkClientTeam"

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     .library(name: "ReactorKit", targets: ["ReactorKit"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/lcg5450/RxSwift.git", .revision("39a1766452d4b78ebb52caa327f29ebc918d23fd")),
-    .package(url: "https://github.com/lcg5450/RxExpect.git", .revision("1108fb25a224dcd8b7cf6861ef30d203fe137c89"))
+    .package(url: "https://github.ncsoft.com/MediaTalkClientTeam/RxSwift.git", .revision("39a1766452d4b78ebb52caa327f29ebc918d23fd")),
+    .package(url: "https://github.ncsoft.com/MediaTalkClientTeam/RxExpect.git", .revision("1108fb25a224dcd8b7cf6861ef30d203fe137c89"))
   ],
   targets: [
     .target(name: "ReactorKit", dependencies: ["ReactorKitRuntime", "RxSwift"]),
