4f8ab14fa13844f78ed77285db506685b5e1b849
ChangGwiLee <gomgom@ncsoft.com>
2020-09-23 22:22:53 +0900

    xcode12

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
     .library(name: "ReactorKit", targets: ["ReactorKit"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.1")),
+    .package(url: "https://github.ncsoft.com/MediaTalkClientTeam/RxSwift.git", .revision("7b410ba9fad7d7201e0a94e692a405c10e976314")),
     .package(url: "https://github.com/ReactorKit/WeakMapTable.git", .upToNextMajor(from: "1.1.0")),
-    .package(url: "https://github.com/devxoul/RxExpect.git", .upToNextMajor(from: "2.0.0"))
+    .package(url: "https://github.ncsoft.com/MediaTalkClientTeam/RxExpect.git", .revision("8f5a17b1ace333338ee5149926f5e8d29c8a1feb"))
   ],
   targets: [
     .target(name: "ReactorKit", dependencies: ["ReactorKitRuntime", "RxSwift", "WeakMapTable"]),
