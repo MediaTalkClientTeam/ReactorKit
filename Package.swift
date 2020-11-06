ada5da3be1c1eeda76557b8aa8288aa56e35b24e
ChangGwiLee <gomgom@ncsoft.com>
2020-11-06 09:18:14 +0900

    rxexpect 주소 변경

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .revision("a82518285848456f404abba1919120161465ea9f")),
     .package(url: "https://github.com/ReactorKit/WeakMapTable.git", .upToNextMajor(from: "1.1.0")),
-    .package(url: "https://github.ncsoft.com/MediaTalkClientTeam/RxExpect.git", .revision("874493562355122ff7831c5ee3981696aa4751a0"))
+    .package(url: "http://MobileAppBuilder@git.korea.ncsoft.corp/scm/media_talk_common/rx-expect.git", .revision("874493562355122ff7831c5ee3981696aa4751a0"))
   ],
   targets: [
     .target(name: "ReactorKit", dependencies: ["ReactorKitRuntime", "RxSwift", "RxCocoa", "RxRelay", "WeakMapTable"]),
