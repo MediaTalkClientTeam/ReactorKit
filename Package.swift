98c46143439a6c5439e2a29d677d16166dc688ab
ChangGwiLee <gomgom@ncsoft.com>
2020-12-08 16:32:16 +0900

    Xcode12 대응 업데이트

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .revision("a82518285848456f404abba1919120161465ea9f")),
-    .package(url: "https://github.com/ReactorKit/WeakMapTable.git", .upToNextMajor(from: "1.1.0")),
-    .package(url: "http://MobileAppBuilder@git.korea.ncsoft.corp/scm/media_talk_common/rx-expect.git", .revision("874493562355122ff7831c5ee3981696aa4751a0"))
+    .package(url: "https://github.com/ReactorKit/WeakMapTable.git", .revision("9580560169b4b48ba2affe7badba6a7f360495f4")),
+    .package(url: "https://github.com/MediaTalkClientTeam/RxExpect.git", .revision("874493562355122ff7831c5ee3981696aa4751a0"))
   ],
   targets: [
     .target(name: "ReactorKit", dependencies: ["ReactorKitRuntime", "RxSwift", "RxCocoa", "RxRelay", "WeakMapTable"]),
